fe0086b5082c8e8e2b4583c9a356f9f563ff9467
David Dominguez <dominguezdavid396@gmail.com>
2024-01-10 09:22:12 -0600

    Volumen Radio

---
+++ Radio.java
@@ -0,0 +1,18 @@
+/**
+ * Radio
+ */
+public class Radio {
+
+    public int volumen = 0;
+
+    public int getVol() {
+        return volumen;
+    }
+
+    public void setVol(int volumen) {
+        this.volumen = volumen;
+    }
+
+
+    
+}
