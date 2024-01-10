c17beeeaacf0268771b7b08f995991ac13d57129
Iris <irisale.ayala@gmail.com>
2024-01-10 09:14:56 -0600

    clases

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public Main(){
+        boolean salir = false;
+        while(!salir){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        Main inicio = new Main();
+    }
+}
