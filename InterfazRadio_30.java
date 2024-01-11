4d73a5872e7e7303d3f1cffa6cf20ec522973688
Jonathan Díaz <77712004+Jonialen@users.noreply.github.com>
2024-01-10 20:11:01 -0600

    Estructura

--- InterfazRadio_30.java
+++ InterfazRadio_30.java
@@ -1,4 +1,25 @@
-public class InterfazRadio_30 {
+import java.util.ArrayList;
+
+public interface InterfazRadio_30 {
+
     public static final int AM = 0;
     public static final int FM = 1;
+
+    public void setEncendido(boolean encendido);
+
+    public void setVolumen(int volumen);
+
+    public void setEstacionBanda(float frecuencia, int banda);
+
+    public boolean getEncendido();
+
+    public float getEstacion();
+
+    public int getBanda();
+
+    public int getVolumen();
+
+    public void guardarEstacion(float frecuencia, int banda, int posicion);
+
+    public <E> ArrayList<E> recuperarEstacion(int posicion);
 }
--- Radio.java
+++ Radio.java
@@ -1,11 +1,49 @@
-public class Radio {
+import java.util.ArrayList;
 
+public class Radio implements InterfazRadio_30 {
+    protected final String marca = "Sansun";
+    public static final int AM = 0;
+    public static final int FM = 1;
     private boolean encendido;
-    protected String marca;
-    private int volumen;
+    private int banda;
     private float estacion;
+    private int volumen;
 
+    public boolean getEncendido() {
+        return encendido;
+    }
 
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+    }
 
+    public int getBanda() {
+        return banda;
+    }
 
+    public float getEstacion() {
+        return estacion;
+    }
+
+    public void setEstacionBanda(float frecuencia, int banda) {
+        this.estacion = frecuencia;
+        this.banda = banda;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
+
+    public void guardarEstacion(float frecuencia, int banda, int posicion) {
+
+    }
+
+    public <E> ArrayList<E> recuperarEstacion(int posicion) {
+        return null;
+
+    }
 }
