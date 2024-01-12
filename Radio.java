a52bb1246f8cfde8fe374c107273881ce5d96c64
David Dominguez <dominguezdavid396@gmail.com>
2024-01-11 20:43:45 -0600

    delimitacion de volumen y Testings

--- Radio.java
+++ Radio.java
@@ -1,18 +1,47 @@
 /**
  * Radio
+ * @author David Dominguez
+ * @version 1.0
+ * @since 2024-01-11
  */
 public class Radio {
 
-    public int volumen = 0;
+    private int volumen;
 
     public int getVol() {
         return volumen;
     }
 
     public void setVol(int volumen) {
-        this.volumen = volumen;
+
+        if (volumen < 0) {
+            this.volumen = 0;
+        } else if (volumen > 100) {
+            this.volumen = 100;
+        } else {
+            this.volumen = volumen;
+        }
     }
 
+    public void volUp(){
+        
+        if (volumen < 100) {
+            this.volumen = volumen + 1;
 
-    
+            if (volumen > 100) {
+                this.volumen = 100;
+            }
+        }
+    }
+
+    public void volDown(){
+
+        if (volumen > 0) {
+            this.volumen = volumen - 1;
+
+            if (volumen < 0) {
+                this.volumen = 0;
+            }
+        }
+    }
 }
